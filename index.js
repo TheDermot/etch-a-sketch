4252ba78256435e4c614a8e70479de9111525c29
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-14 13:18:38 -0500

    Implemented event listener to track if mousedown
    To create a press down and draw effect I set a variable to track if
    the mouse is down or not through a document level event listener

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Set default grid size
 const gridSize = 16;
 const etchContainer = document.querySelector(".etch-grid");
 
@@ -10,12 +11,39 @@ const setGridSize = (gridSize) => {
     let squareSize = `1 1 calc(100%/${gridSize})`;
     square.classList.add("square");
     square.style.flex = squareSize;
+
+    //set color
+    square.addEventListener("mouseenter", (e) => {
+      if (mouseDown) draw(e);
+    });
+    //Append square
     etchContainer.appendChild(square);
   }
 };
+
 setGridSize(gridSize);
+
 //get user input
 const userSizeSelection = document.getElementById("grid-dimension");
 userSizeSelection.addEventListener("input", (event) => {
-  setGridSize(event.target.value);
+  setGridSize(event.target.value); //references element event was triggered by
 });
+
+//draw
+const draw = (e) => {
+  e.target.style.backgroundColor = "black";
+};
+
+//track if mouse is down
+let mouseDown = false;
+document.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+// Prevent the grid from being dragged like an image/file
+etchContainer.addEventListener("dragstart", (event) => {
+  event.preventDefault();
+});
+
